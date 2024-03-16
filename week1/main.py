93b6e20934ef602547b0351e5f1478ec9de977fd
AlexX314159 <162941271+AlexX314159@users.noreply.github.com>
2024-03-15 19:17:26 -0700

    Update main.py

--- week1/main.py
+++ week1/main.py
@@ -20,12 +20,12 @@ def check_winner(board):
         if row[0] == row[1] and row[0] == row[2] and row [0]!= ' ':
             return True
     for col in range(3):
-        if board[0][col] == board[1][col] and row[0] == board[2][col] and row[0]!= ' ':
+        if board[0][col] == board[1][col] and board[0][0] == board[2][col] and row[0]!= ' ':
             return True
 
-    if board[1][1] == board[2][2] and row[0] == board[0][0] != ' ':
+    if board[1][1] == board[0][0] == board[2][2] and board[0][0] != ' ':
         return True
-    if board[2][0] == board[0][2] == board[1][1] != ' ':
+    if board[2][0] == board[0][2] and board[0][0] == board[1][1] != ' ':
         return
 
         
