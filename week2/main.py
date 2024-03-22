48c452c0a00433a3f25dfb8d324705ea4406f594
purplecarrot8 <162940713+purplecarrot8@users.noreply.github.com>
2024-03-21 21:42:43 -0700

    Update main.py
    I finished the homework!! There was an error in line 15 (check winner code) that I fixed. Yay! I also added the code to check when the input was between 1 and 3, and when there was a tie.

--- week2/main.py
+++ week2/main.py
@@ -1,7 +1,7 @@
 def print_board(board):
     for row in board:
         print(" | ".join(row))
-        print("-" * 5)
+        print("-" * 8)
 
 
 def check_winner(board):
@@ -12,7 +12,7 @@ def check_winner(board):
 
     # Check columns
     for col in range(len(board[0])):
-        if all(board[row][col] == board[0][col] and board[col][0] != ' ' for row in range(len(board))):
+        if all(board[row][col] == board[0][col] and board[0][col] != ' ' for row in range(len(board))):
             return True
 
     # Check diagonals
@@ -26,12 +26,73 @@ def check_winner(board):
 
 
 def test_check_winner():
+    player = 'X'
+    err=0
+    # case 1, rows
+    for r in range(3):
+        board = [[' ' for _ in range(3)] for _ in range(3)]
+        board[r][0]=player
+        board[r][1]=player
+        board[r][2]=player
+        if check_winner(board)==False:
+            print('Error: winner not detected')
+            print_board(board)
+            err = 1
+        
+    # case 2, cols  
+    for r in range(3):
+        board = [[' ' for _ in range(3)] for _ in range(3)]
+        board[0][r]=player
+        board[1][r]=player
+        board[2][r]=player
+        if check_winner(board)==False:
+            print('Error: winner not detected')
+            print_board(board)
+            err = 1
+    
+    # case 3, diagonal
+        board = [[' ' for _ in range(3)] for _ in range(3)]
+        board[0][0]=player
+        board[1][1]=player
+        board[2][2]=player
+        if check_winner(board)==False:
+            print('Error: winner not detected')
+            print_board(board)
+            err = 1
+    # case 4, diagonal
+        board = [[' ' for _ in range(3)] for _ in range(3)]
+        board[0][2]=player
+        board[1][1]=player
+        board[2][0]=player
+
+        if check_winner(board)==False:
+            print('Error: winner not detected')
+            print_board(board)
+            err = 1
+    # case 5, tie
+        board = [[' ' for _ in range(3)] for _ in range(3)]
+        board[0][0]='X'
+        board[0][1]='O'
+        board[0][2]='X'
+        board[1][1]='O'
+        board[1][0]='X'
+        board[1][2]='O'
+        board[2][2]='X'
+        board[2][1]='X'
+        board[2][0]='O'
+
+        if check_winner(board)==True:
+            print('Oops:looks like you got a tie!')
+            print_board(board)
+            err=1
+
     '''
     Check for all possible win conditions and make sure that our check_winner function is correctly 
-    declaring that a win condition is achived.   Also test that if given a draw condition, the code
+    declaring that a win condition is achieved.   Also test that if given a draw condition, the code
     correctly recognizes that there's no winner.
     '''
-    print("All test cases passed!")
+    if err==0:
+        print("All test cases passed!")
 
 
 def tic_tac_toe():
@@ -39,8 +100,17 @@ def tic_tac_toe():
     player = 'X'
     while True:
         print_board(board)
-        row = int(input(f"Player {player}, choose row (1-3): ")) - 1
-        col = int(input(f"Player {player}, choose column (1-3): ")) - 1
+
+        while True:
+            row = int(input(f"Player {player}, choose row (1-3): ")) - 1
+            if row >=0 and row <= 2:
+                break
+
+        while True:
+            col = int(input(f"Player {player}, choose column (1-3): ")) - 1
+            if col >=0 and col <=2:
+                break
+
 
         if board[row][col] == ' ':
             board[row][col] = player
