6a275a98df95dcc29eadb6246040cb64280f2c38
AlexX314159 <162941271+AlexX314159@users.noreply.github.com>
2024-03-27 18:33:08 -0700

    Update main.py

--- week2/main.py
+++ week2/main.py
@@ -1,4 +1,5 @@
-import tkenter
+import tkenter as tk
+import 
 def print_board(board):
     for row in board:
         print(" | ".join(row))
@@ -40,16 +41,54 @@ def test_check_winner():
     assert check_winner([['X','O','X'],['X','X','O'],['O','X','O']]) == False
     print("All test cases passed!")
 
-def onclick(row, col):
+def update_gui():
+    for i in range(3):
+        for j in range(3):
+            buttons[i][j].config(text=board[i][j])
+
+'''
+complete the function on_click
+function should
+1. Update the board and synchronize with the same update as buttons
+2. Check for winner and prompt if a winner is found 
+3. Check for ties and proompt
+4. Check that the move is valid.  i.e. the box is not already taken
+'''
+def on_click(row, col):
     if board[row][col]==' ':
-        board[row][col] = xxhkjfxh
+        board[row][col] = player
+        update_gui()
         if check_winner(board):
             reset_game()
             return
-        if all(board[n][m] !=' ' for n in range(3) for m in range(3)):
+        if all(board[n][m] !=' ' for n in range(3) for m in range (3):
             reset_game()
             return
     else:
+        
+
+'''
+complete the logic for reset_game()
+this should allow a new game to be played
+'''
+def reset_game():
+    for i in range(3):
+        for j in range(3):
+            board[i][j] = ' '
+            buttons[i][j].
+ 
+
+def create_gui():
+    global buttons
+    root = tk.Tk()
+    root.title("Tic Tac Toe")
+    buttons = [[None]*3 for _ in range(3)]
+    for i in range(3):
+        for j in range(3):
+            buttons[i][j] = tk.Button(root, text=' ', font=('Arial', 30), width=3, height=1, command=lambda row=i, col=j: on_click(row, col))
+            buttons[i][j].grid(row=i, column=j)
+    return root
+
         
             
 
